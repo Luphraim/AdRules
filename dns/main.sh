9425e947a88b173c50a7040d77a7c470527edf78
PhoenixLjw <PhoenixLjw@outlook.com>
2021-12-21 20:14:22 +0800

    update dns rules

--- dns/main.sh
+++ dns/main.sh
@@ -14,7 +14,7 @@ curl -o i8.txt https://paulgb.github.io/BarbBlock/blacklists/hosts-file.txt
 # Start Merge and Duplicate Removal
 cat i*.txt > mergd.txt
 cat mergd.txt | grep -v '^!' | grep -v '^！' | grep -v '^# ' | grep -v '^# ' | grep -v '^\[' | grep -v '^\【' > all.txt
-sed -i 's/"0.0.0.0"/"127.0.0.1"/g' all.txt
+sed -i 's/0.0.0.0/127.0.0.1/g' all.txt
 sort -n all.txt | uniq > rules.txt
 
 # Start Count Rules
