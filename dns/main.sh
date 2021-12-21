9b85a6fd98970bd92aeed9124890e5b3d1b2c665
PhoenixLjw <PhoenixLjw@outlook.com>
2021-12-21 18:53:22 +0800

    add all filters

---
+++ dns/main.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+cd dns/rules
+
+# Start Download
+curl -o i1.txt https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
+curl -o i2.txt https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts
+curl -o i3.txt https://raw.githubusercontent.com/badmojr/1Hosts/master/lite/adblock.txt
+curl -o i4.txt https://cdn.jsdelivr.net/gh/neoFelhz/neohosts@gh-pages/full/hosts.txt
+curl -o i5.txt https://adaway.org/hosts.txt
+curl -o i6.txt https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-easylist.txt
+curl -o i7.txt https://raw.githubusercontent.com/neodevpro/neodevhost/master/lite_host
+curl -o i8.txt https://paulgb.github.io/BarbBlock/blacklists/hosts-file.txt
+
+# Start Merge and Duplicate Removal
+cat i*.txt > mergd.txt
+cat mergd.txt | grep '^|' | grep -v './' | grep -v '.\$' > block.txt
+cat mergd.txt | grep '^@' | grep -v './' | grep -v '.\$' > allow.txt
+cat block.txt allow.txt > all.txt
+sort -n all.txt | uniq > rules.txt
+
+# Start Count Rules
+num=`cat rules.txt | wc -l`
+
+# Merge title, date, and rules
+echo "! Version: `date +"%Y-%m-%d %H:%M:%S"`" >> tpdate.txt
+echo "! Total count: $num" >> tpdate.txt
+cat title.dd tpdate.txt rules.txt > ../../dns.txt
+rm *.txt
+cd ../../
+exit
---
+++ pc/main.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+cd pc/rules
+
+# Start Download
+curl -o i1.txt https://raw.githubusercontent.com/o0HalfLife0o/list/master/ad-pc.txt
+curl -o i2.txt https://raw.githubusercontent.com/o0HalfLife0o/list/master/ad-edentw.txt
+curl -o i3.txt https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt
+curl -o i4.txt https://raw.githubusercontent.com/banbendalao/ADgk/master/kill-baidu-ad.txt
+curl -o i5.txt https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt
+curl -o i6.txt https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt
+curl -o i7.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock.txt
+curl -o i8.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_privacy.txt
+curl -o i9.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/annoyances.txt
+curl -o i10.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt
+curl -o i11.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt
+curl -o i12.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt
+curl -o i13.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/unbreak.txt
+curl -o i14.txt https://raw.githubusercontent.com/neodevpro/neodevhost/master/lite_adblocker
+curl -o i15.txt https://paulgb.github.io/BarbBlock/blacklists/hosts-file.txt
+curl -o i16.txt https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-adguard.txt
+# curl -o i17.txt https://www.i-dont-care-about-cookies.eu/abp/
+
+# Start Merge and Duplicate Removal
+cat i*.txt > mergd.txt
+cat mergd.txt | grep -v '^!' | grep -v '^！' | grep -v '^# ' | grep -v '^# ' | grep -v '^\[' | grep -v '^\【' > all.txt
+sort -n all.txt | uniq > rules.txt
+
+# Start Count Rules
+num=`cat rules.txt | wc -l`
+
+# Start Add title and date
+echo "! Version: `date +"%Y-%m-%d %H:%M:%S"`" >> tpdate.txt
+echo "! Total count: $num" >> tpdate.txt
+cat title.dd tpdate.txt user-rules.dd rules.txt > ../../filter.txt
+rm *.txt
+cd ../../
+exit
