2d146dde10ef5a484f42f3dc566907aed76c2010
PhoenixLjw <PhoenixLjw@outlook.com>
2022-05-02 14:46:45 +0800

    update list of upstream rules

--- utils/deduplication.sh
+++ utils/deduplication.sh
@@ -247,10 +247,11 @@ cat tmp-adblock.txt adblock_lite*.txt \
  
 # 合并HOSTS过滤规则并转化为DNS过滤规则
 cat hosts*.txt \
+ | grep -E -v '^((\!)|(\！)|(\[)).*' \
  | sed s/127.0.0.1/0.0.0.0/ \
  | sed s/::/0.0.0.0/g \
  | sed 's/  / /' \
- | grep -E '^(0.0.0.0 ).*'
+ | grep -E '^(0\.0\.0\.0\ ).*'
  | sort -u > pre-hosts.txt
 cat pre-hosts.txt \
  | sed 's/0.0.0.0 /||/g' \
